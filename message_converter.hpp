02557fc3b4532d63fc752dbf5ee3cdecbd73e234
kali pso <kalipso@c3d2.de>
2019-01-06 18:04:35 +0000

    Adding proper Peer, Header and Message object. Testing for each of them is implemented. Adding MessageConverter and  ProtobufMessageConverter. They allow us to change serialization algorithm. Testing is also implemented.

--- message_converter.hpp
+++ message_converter.hpp
@@ -13,35 +13,29 @@
 #include <google/protobuf/util/delimited_message_util.h>
 
 //TODO: non boost possible?
-typedef std::vector<boost::uint8_t> data_buffer;
+typedef std::vector<boost::uint8_t> DataBuffer;
 typedef std::shared_ptr<Message> MessagePtr;
 
 class MessageConverter
 {
 public:
-    MessageConverter ();
-    virtual ~MessageConverter();
+    virtual ~MessageConverter() {}
 
-    virtual const MessagePtr MessageFromSerialized(const data_buffer& buf) = 0;
-    virtual const MessagePtr SerializedFromMessage(const Message& message) = 0;
+    virtual const MessagePtr MessageFromSerialized(const DataBuffer& buf) const = 0;
+    virtual const DataBuffer SerializedFromMessage(const MessagePtr message) const = 0;
 };
 
-class ProtobufMessageConverter : MessageConverter
+class ProtobufMessageConverter : public MessageConverter
 {
 public:
-    ProtobufMessageConverter ();
-    ~ProtobufMessageConverter() {}
-
-    void test(){
-        std::cout << "test" << '\n';
-    }
-    const MessagePtr MessageFromSerialized(const data_buffer& buf) override
+    const MessagePtr MessageFromSerialized(const DataBuffer& buf) const override
     {
         //create a Protobuf Message
         auto protobuf_message = std::make_shared<Request>();
-        //fill Message with data by parsing from data_buffer
-        protobuf_message->ParseFromArray(&buf, buf.size()); //TODO: could fail
+        //fill Message with data by parsing from DataBuffer
+        protobuf_message->ParseFromArray(&buf[0], buf.size()); //TODO: could fail
 
+        //Get protobuf_header and create Header from it
         auto protobuf_header = protobuf_message->commonheader();
         Header header(protobuf_header.t_flag(),
                       protobuf_header.ttl(),
@@ -51,9 +45,12 @@ public:
                       protobuf_header.version(),
                       protobuf_header.response_code());
 
+        //Create MessagePtr and set the header
         MessagePtr message = std::make_shared<Message>();
         message->set_header(header);
 
+        //Iterate over every protobuf_peer, create Peer object from it
+        //and add it to our message
         const int protobuf_peer_size = protobuf_message->peerinfo_size();
         for(int i = 0; i < protobuf_peer_size; i++){
             auto protobuf_peer = protobuf_message->peerinfo(i);
@@ -62,13 +59,38 @@ public:
                       protobuf_peer.peer_port());
             message->add_peer(peer);
         }
-        std::cout << "lo" << std::endl;
 
         return message;
     }
 
-    const MessagePtr SerializedFromMessage(const Message& message) override
-    {}
+    const DataBuffer SerializedFromMessage(const MessagePtr message) const override
+    {
+        auto protobuf_message = std::make_shared<Request>();
+        auto protobuf_header = protobuf_message->mutable_commonheader();
+        auto peerpaste_header = message->get_header();
+        auto peerpaste_peers = message->get_peers();
+
+        protobuf_header->set_t_flag(peerpaste_header.get_t_flag());
+        protobuf_header->set_ttl(peerpaste_header.get_ttl());
+        protobuf_header->set_message_length(peerpaste_header.get_message_length());
+        protobuf_header->set_request_type(peerpaste_header.get_request_type());
+        protobuf_header->set_transaction_id(peerpaste_header.get_transaction_id());
+        protobuf_header->set_version(peerpaste_header.get_version());
+        protobuf_header->set_response_code(peerpaste_header.get_response_code());
+
+        for(const auto peer : peerpaste_peers){
+            auto protobuf_peer = protobuf_message->add_peerinfo();
+            protobuf_peer->set_peer_id(peer.get_id());
+            protobuf_peer->set_peer_ip(peer.get_ip());
+            protobuf_peer->set_peer_port(peer.get_port());
+        }
+
+        std::vector<uint8_t> buf;
+        unsigned protobuf_message_size = protobuf_message->ByteSize();
+        buf.resize(protobuf_message_size);
+        protobuf_message->SerializeToArray(&buf[0], protobuf_message_size);
+        return buf;
+    }
 
 private:
     /* data */
---
+++ test/test.cpp
@@ -0,0 +1,217 @@
+#include "header.hpp"
+#include "peer.hpp"
+#include "message.hpp"
+#include "message_converter.hpp"
+#include "cryptowrapper.hpp"
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch.hpp>
+
+TEST_CASE("Testing Header Object", "[peerpaste::message::header]")
+{
+    bool t_flag = true;
+    uint32_t ttl = 1;
+    uint64_t message_length = 10;
+    std::string request_type = "query";
+    std::string transaction_id = "123ABC";
+    std::string version = "1.0.0";
+    std::string response_code = "UNKNOWN";
+
+    Header header(t_flag, ttl, message_length, request_type,
+                  transaction_id, version, response_code);
+
+    REQUIRE(header.get_t_flag() == true);
+    REQUIRE(header.get_ttl() == 1);
+    REQUIRE(header.get_message_length() == 10);
+    REQUIRE(header.get_request_type() == "query");
+    REQUIRE(header.get_transaction_id() == "123ABC");
+    REQUIRE(header.get_version() == "1.0.0");
+    REQUIRE(header.get_response_code() == "UNKNOWN");
+
+    header.set_t_flag(false);
+    header.set_ttl(2);
+    header.set_message_length(11);
+    header.set_request_type("join");
+    header.set_transaction_id("456DEF");
+    header.set_version("2.0.1");
+    header.set_response_code("KNOWN");
+
+    REQUIRE(header.get_t_flag() == false);
+    REQUIRE(header.get_ttl() == 2);
+    REQUIRE(header.get_message_length() == 11);
+    REQUIRE(header.get_request_type() == "join");
+    REQUIRE(header.get_transaction_id() == "456DEF");
+    REQUIRE(header.get_version() == "2.0.1");
+    REQUIRE(header.get_response_code() == "KNOWN");
+}
+
+TEST_CASE("Testing Peer Object", "[peerpaste::message::peer]")
+{
+    std::string id = "123ABC";
+    std::string ip = "10.0.0.1";
+    std::string port = "1337";
+
+    Peer peer(id, ip, port);
+
+    REQUIRE(peer.get_id() == "123ABC");
+    REQUIRE(peer.get_ip() == "10.0.0.1");
+    REQUIRE(peer.get_port() == "1337");
+
+    peer.set_id("456DEF");
+    peer.set_ip("20.2.2.2");
+    peer.set_port("4242");
+
+    REQUIRE(peer.get_id() == "456DEF");
+    REQUIRE(peer.get_ip() == "20.2.2.2");
+    REQUIRE(peer.get_port() == "4242");
+}
+
+TEST_CASE("Testing Message Object", "[peerpaste::message]")
+{
+    std::string id = "123ABC";
+    std::string ip = "10.0.0.1";
+    std::string port = "1337";
+
+    Peer peer(id, ip, port);
+
+    bool t_flag = true;
+    uint32_t ttl = 1;
+    uint64_t message_length = 10;
+    std::string request_type = "query";
+    std::string transaction_id = "123ABC";
+    std::string version = "1.0.0";
+    std::string response_code = "UNKNOWN";
+
+    Header header(t_flag, ttl, message_length, request_type,
+                  transaction_id, version, response_code);
+
+    Message message;
+    message.set_header(header);
+    message.add_peer(peer);
+    message.add_peer(peer);
+    message.add_peer(peer);
+
+    auto header_ = message.get_header();
+
+    REQUIRE(header_.get_t_flag() == true);
+    REQUIRE(header_.get_ttl() == 1);
+    REQUIRE(header_.get_message_length() == 10);
+    REQUIRE(header_.get_request_type() == "query");
+    REQUIRE(header_.get_transaction_id() == "123ABC");
+    REQUIRE(header_.get_version() == "1.0.0");
+    REQUIRE(header_.get_response_code() == "UNKNOWN");
+
+    auto peers = message.get_peers();
+
+    REQUIRE(peers.size() == 3);
+    std::vector<Peer> new_peers;
+    for(auto peer : peers){
+        REQUIRE(peer.get_id() == "123ABC");
+        REQUIRE(peer.get_ip() == "10.0.0.1");
+        REQUIRE(peer.get_port() == "1337");
+
+        peer.set_id("456DEF");
+        peer.set_ip("20.2.2.2");
+        peer.set_port("4242");
+        new_peers.push_back(peer);
+    }
+
+    message.set_peers(new_peers);
+    for(const auto peer : message.get_peers()){
+        REQUIRE(peer.get_id() == "456DEF");
+        REQUIRE(peer.get_ip() == "20.2.2.2");
+        REQUIRE(peer.get_port() == "4242");
+    }
+}
+
+TEST_CASE( "Testing ProtobufMessageConverter", "[peeraste::MessageConverter]" )
+{
+    //Create a protobuf message
+    auto protobuf_message = std::make_shared<Request>();
+
+    //Add header
+    auto protobuf_header = protobuf_message->mutable_commonheader();
+    protobuf_header->set_t_flag(true);
+    protobuf_header->set_ttl(10);
+    protobuf_header->set_message_length(15);
+    protobuf_header->set_request_type("query");
+    protobuf_header->set_transaction_id("secret");
+    protobuf_header->set_version("1.0.0");
+    protobuf_header->set_response_code("UNKNOWN");
+
+    //Add first peer
+    auto protobuf_peerinfo1 = protobuf_message->add_peerinfo();
+    protobuf_peerinfo1->set_peer_id("123ABC");
+    protobuf_peerinfo1->set_peer_ip("123ABC");
+    protobuf_peerinfo1->set_peer_port("123ABC");
+
+    //Add second peer
+    auto protobuf_peerinfo2 = protobuf_message->add_peerinfo();
+    protobuf_peerinfo2->set_peer_id("321CBA");
+    protobuf_peerinfo2->set_peer_ip("321CBA");
+    protobuf_peerinfo2->set_peer_port("321CBA");
+
+    //Create buffer and serialize protobuf message into it
+    std::vector<uint8_t> buf;
+    unsigned protobuf_message_size = protobuf_message->ByteSize();
+    buf.resize(protobuf_message_size);
+    protobuf_message->SerializeToArray(&buf[0], protobuf_message_size);
+
+    //Create converter and create a Message from the given buffer
+    ProtobufMessageConverter converter;
+    auto peerpaste_message = converter.MessageFromSerialized(buf);
+    peerpaste_message->print();
+
+    //Check if header contains right information
+    auto peerpaste_header = peerpaste_message->get_header();
+    REQUIRE(peerpaste_header.get_t_flag() == true);
+    REQUIRE(peerpaste_header.get_ttl() == 10);
+    REQUIRE(peerpaste_header.get_message_length() == 15);
+    REQUIRE(peerpaste_header.get_request_type() == "query");
+    REQUIRE(peerpaste_header.get_transaction_id() == "secret");
+    REQUIRE(peerpaste_header.get_version() == "1.0.0");
+    REQUIRE(peerpaste_header.get_response_code() == "UNKNOWN");
+
+    //Check if peers contain right information
+    auto peerpaste_peers = peerpaste_message->get_peers();
+    REQUIRE(peerpaste_peers.size() == 2);
+    REQUIRE(peerpaste_peers[0].get_id() == "123ABC");
+    REQUIRE(peerpaste_peers[0].get_ip() == "123ABC");
+    REQUIRE(peerpaste_peers[0].get_port() == "123ABC");
+    REQUIRE(peerpaste_peers[1].get_id() == "321CBA");
+    REQUIRE(peerpaste_peers[1].get_ip() == "321CBA");
+    REQUIRE(peerpaste_peers[1].get_port() == "321CBA");
+
+    //Check if the created buffer is the same when generated by message_converter
+    REQUIRE(buf == converter.SerializedFromMessage(peerpaste_message));
+}
+
+TEST_CASE( "Testing util::between()", "[util::between()]" )
+{
+    std::string a = "a";
+    std::string b = "b";
+    std::string c = "c";
+    std::string num1 = "1";
+    std::string num2 = "2";
+    std::string num3 = "3";
+
+    REQUIRE(util::between(a, b, c) == true);
+    REQUIRE(util::between(b, b, c) == false);
+    REQUIRE(util::between(c, b, c) == true);
+    REQUIRE(util::between(a, b, a) == true);
+    REQUIRE(util::between(c, a, b) == true);
+    REQUIRE(util::between(num2, b, num1) == true);
+    REQUIRE(util::between(num2, b, num3) == false);
+    REQUIRE(util::between(num1, num2, num3) == true);
+    REQUIRE(util::between(num3, num2, num1) == false);
+    REQUIRE(util::between(num3, c, num2) == true);
+    REQUIRE(util::between(c, num1, a) == true);
+    REQUIRE(util::between(num1, num3, c) == true);
+}
