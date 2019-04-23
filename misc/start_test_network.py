c21c737a0e549c021246110e80bb3b1d4b198553
kali pso <kalipso@c3d2.de>
2019-04-23 15:21:55 +0000

    moving python/ to misc/ and clearing that folder up

--- misc/start_test_network.py
+++ misc/start_test_network.py
@@ -2,7 +2,7 @@ import os
 from time import sleep
 # os.system("../build/node 1337 &")
 for i in range(1338, 1348):
-    command = "../build/peerpaste -j localhost %s -p %s --debug &" % (( i-1 ), i)
+    command = "../build/peerpaste -j localhost %s -p %s &" % (( i-1 ), i)
     print(command)
     os.system(command)
     sleep(0.4)
