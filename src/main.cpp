d0de5c2daa97b826a41b9be757cd74c1fb4c3c95
kali pso <kalipso@c3d2.de>
2019-08-11 23:05:31 +0000

    Adding unencrypted put/get to commandline tool

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,8 @@ int main(int argc, char** argv)
         ("join,j", po::value<std::vector<std::string>>()->multitoken()->composing(), "IP and Port of Host to connect to")
         ("put", po::value<std::vector<std::string>>()->multitoken()->composing(), "Path to text file")
         ("get", po::value<std::vector<std::string>>()->multitoken()->composing(), "Hash of a stored Paste")
+        ("put-unencrypted", po::value<std::vector<std::string>>()->multitoken()->composing(), "Path to text file")
+        ("get-unencrypted", po::value<std::vector<std::string>>()->multitoken()->composing(), "Hash of a stored Paste")
         ("verbose", "show additional information")
         ("create", "create new ring")
         ("log-messages", "log messages to files")
@@ -147,6 +149,17 @@ int main(int argc, char** argv)
             auto future_ = peerpaste.async_put(ip, port, data_encrypted);
             future_.wait();
             std::cout << data_key << future_.get() << std::endl;
+        } else if(vm.count("put-unencrypted")) {
+            auto vec = vm["put-unencrypted"].as<std::vector<std::string>>();
+            auto ip = vec.at(0);
+            auto port = vec.at(1);
+            std::string filename = vec.at(2);
+            std::ifstream f(filename);
+            std::string data((std::istreambuf_iterator<char>(f)),
+                             std::istreambuf_iterator<char>());
+            auto future_ = peerpaste.async_put(ip, port, data);
+            future_.wait();
+            std::cout << future_.get() << std::endl;
         } else if(vm.count("get")) {
             auto vec = vm["get"].as<std::vector<std::string>>();
             auto ip = vec.at(0);
@@ -157,6 +170,14 @@ int main(int argc, char** argv)
             auto future_ = peerpaste.async_get(ip, port, data_hash);
             future_.wait();
             std::cout << util::decrypt(data_key, future_.get()) << std::endl;
+        } else if(vm.count("get-unencrypted")) {
+            auto vec = vm["get-unencrypted"].as<std::vector<std::string>>();
+            auto ip = vec.at(0);
+            auto port = vec.at(1);
+            auto data = vec.at(2);
+            auto future_ = peerpaste.async_get(ip, port, data);
+            future_.wait();
+            std::cout << future_.get() << std::endl;
         } else {
             //only call msg_handler.run() when this node
             //should be part of the ring
