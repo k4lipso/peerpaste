ae2ede713d7e76f6888a7d9a61ce121552cb22c2
kalipso <kalipso@c3d2.de>
2020-01-16 22:48:33 +0100

    adjust for build on nix
    includes temporary boost library path

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
   name = "env";
-  nativeBuildInputs = [ python3 python37Packages.klein gcc9 ccls cmake lldb gdb ];
-  buildInputs = [ protobuf cryptopp boost boost-build libsodium ];
+  nativeBuildInputs = [ pkgconfig python3 python37Packages.klein gcc ccls cmake lldb gdb ];
+  buildInputs = [ protobuf cryptopp boost171 boost-build libsodium doxygen ];
 }
