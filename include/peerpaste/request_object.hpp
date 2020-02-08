cd2b5dfd22e9f6272143417f34a3a0dcd3227dd7
kalipso <kalipso@c3d2.de>
2020-02-08 14:55:21 +0100

    prototyping

--- include/peerpaste/request_object.hpp
+++ include/peerpaste/request_object.hpp
@@ -16,7 +16,8 @@ using MessagePtr = std::shared_ptr<Message>;
 using PeerPtr = std::shared_ptr<Peer>;
 using SessionPtr = std::shared_ptr<Session>;
 using RequestObjectUPtr = std::unique_ptr<RequestObject>;
-using HandlerFunction = std::function<void(RequestObjectUPtr)>;
+using HandlerFunctionDeprecated = std::function<void(RequestObjectUPtr)>;
+using HandlerFunction = std::function<void(RequestObject)>;
 using DataPromise = std::shared_ptr<std::promise<std::string>>;
 
 enum class MessageType
@@ -25,199 +26,211 @@ enum class MessageType
   NOTIFICATION,
 };
 
+template<typename Handler>
 struct HandlerObject
 {
+//  HandlerObject() {}
+  HandlerObject(std::string correlation_id, const Handler& handler)
+    : correlation_id_(std::move(correlation_id))
+    , handler_(handler)
+  {}
+
+  HandlerObject(const HandlerObject& other)
+    : correlation_id_(other.correlation_id_)
+    ,	handler_(other.handler_)
+  {}
+
+//  HandlerObject(HandlerObject&& other)
+//    : correlation_id_(std::move(other.correlation_id_))
+//    ,	handler_(other.handler_)
+//  {}
+
+  bool operator<(const HandlerObject<Handler>& other) const
+  {
+    return correlation_id_ < other.correlation_id_;
+  }
+
   std::string correlation_id_;
-  std::function<void(RequestObject)> handler_;
+  Handler handler_;
 };
 
 class RequestObject
 {
 public:
-    RequestObject(MessageType type = MessageType::UNKNOWN)
-      : type_(type)
-    {
-        start_ = std::chrono::steady_clock::now();
-    }
+  RequestObject(MessageType type = MessageType::UNKNOWN)
+    : type_(type)
+  {
+      start_ = std::chrono::steady_clock::now();
+  }
 
-    ~RequestObject () {}
+  /*
+   * Calls the optional handlerfunction with the
+   * given request object
+   * The Idea is that you create a Request, set a handler
+   * and store it in an open_requests container
+   * As soon as you get a response with a matching
+   * corelational ID you pass the response object
+   * to this call function of the original request object
+   * which will execute the function with the response as an
+   * argument
+   */
+  void call(RequestObjectUPtr request)
+  {
+      //throw exeption if no value
+      if(handler_.has_value()) handler_.value()(std::move(request));
+  }
 
-    /* static RequestObject create_request_object(Message msg, */
-    /*                                            std::variant<Peer, SessionPtr> connection) */
-    /* { */
-    /*     RequestObject request; */
-    /*     request.set_message(std::make_shared<Message>(msg)); */
-    /*     set_connection(request, connection); */
+  void call(std::shared_ptr<RequestObject> request)
+  {
+      //throw exeption if no value
+      auto unique_request = std::make_unique<RequestObject>(*request.get());
+      if(handler_.has_value()) handler_.value()(std::move(unique_request));
+  }
 
-    /*     return request; */
-    /* } */
+  void set_handler(HandlerFunctionDeprecated handler)
+  {
+      handler_ = handler;
+  }
 
-    /* static RequestObject create_request_object(Message msg, */
-    /*                                            std::variant<Peer, SessionPtr> connection, */
-    /*                                            HandlerFunction fct) */
-    /* { */
-    /*     RequestObject request; */
-    /*     request.set_message(std::make_shared<Message>(msg)); */
-    /*     request.set_handler(fct); */
-    /*     set_connection(request, connection); */
+  bool has_handler() const
+  {
+      return handler_.has_value() || handler_object_.has_value();
+  }
 
-    /*     return request; */
-    /* } */
+  const HandlerFunctionDeprecated get_handler() const
+  {
+      return handler_.value();
+  }
 
-    /*
-     * Calls the optional handlerfunction with the
-     * given request object
-     * The Idea is that you create a Request, set a handler
-     * and store it in an open_requests container
-     * As soon as you get a response with a matching
-     * corelational ID you pass the response object
-     * to this call function of the original request object
-     * which will execute the function with the response as an
-     * argument
-     */
-    void call(RequestObjectUPtr request)
-    {
-        //throw exeption if no value
-        if(handler_.has_value()) handler_.value()(std::move(request));
-    }
+  void set_handler_object(HandlerObject<HandlerFunction> handler_object)
+  {
+    handler_object_ = handler_object;
+  }
 
-    void call(std::shared_ptr<RequestObject> request)
-    {
-        //throw exeption if no value
-        auto unique_request = std::make_unique<RequestObject>(*request.get());
-        if(handler_.has_value()) handler_.value()(std::move(unique_request));
-    }
+  bool has_handler_object() const
+  {
+    return handler_object_.has_value();
+  }
 
-    void set_handler(HandlerFunction handler)
-    {
-        handler_ = handler;
-    }
+  HandlerObject<HandlerFunction> get_handler_object() const
+  {
+    return handler_object_.value();
+  }
 
-    bool has_handler() const
-    {
-        return handler_.has_value();
-    }
 
-    const HandlerFunction get_handler() const
-    {
-        return handler_.value();
-    }
+  void set_promise(const DataPromise& data_promise)
+  {
+      data_promise_deprecated_ = data_promise;
+  }
 
-    [[deprecated]]
-    void set_promise(const DataPromise& data_promise)
-    {
-        data_promise_deprecated_ = data_promise;
-    }
+  bool has_promise() const
+  {
+      return data_promise_deprecated_.has_value();
+  }
 
-    [[deprecated]]
-    bool has_promise() const
-    {
-        return data_promise_deprecated_.has_value();
-    }
+  bool set_promise_value(const std::string& value)
+  {
+      if(not has_promise()){
+          util::log(debug, "request_object::set_promise_value: has no promise");
+          return false;
+      }
+      data_promise_deprecated_.value()->set_value(value);
+      return true;
+  }
 
-    [[deprecated]]
-    bool set_promise_value(const std::string& value)
-    {
-        if(not has_promise()){
-            util::log(debug, "request_object::set_promise_value: has no promise");
-            return false;
-        }
-        data_promise_deprecated_.value()->set_value(value);
-        return true;
-    }
+  void set_message(MessagePtr message)
+  {
+      message_ = message;
+  }
 
-    void set_message(MessagePtr message)
-    {
-        message_ = message;
-    }
+  const MessagePtr get_message() const
+  {
+      return message_;
+  }
 
-    const MessagePtr get_message() const
-    {
-        return message_;
-    }
+  void set_connection(PeerPtr peer)
+  {
+      connection_ = peer;
+  }
 
-    void set_connection(PeerPtr peer)
-    {
-        connection_ = peer;
-    }
+  void set_connection(SessionPtr session)
+  {
+      connection_ = session;
+  }
 
-    void set_connection(SessionPtr session)
-    {
-        connection_ = session;
-    }
+  bool is_session() const
+  {
+      return std::holds_alternative<SessionPtr>(connection_);
+  }
 
-    bool is_session() const
-    {
-        return std::holds_alternative<SessionPtr>(connection_);
-    }
+  const SessionPtr get_session() const
+  {
+      return std::get<SessionPtr>(connection_);
+  }
 
-    const SessionPtr get_session() const
-    {
-        return std::get<SessionPtr>(connection_);
-    }
+  const PeerPtr get_peer() const
+  {
+      return std::get<PeerPtr>(connection_);
+  }
 
-    const PeerPtr get_peer() const
-    {
-        return std::get<PeerPtr>(connection_);
-    }
+  const std::string get_correlational_id() const noexcept
+  {
+      return message_->get_correlational_id();
+  }
 
-    const std::string get_correlational_id() const noexcept
-    {
-        return message_->get_correlational_id();
-    }
+  const std::string get_request_type() const noexcept
+  {
+      return message_->get_request_type();
+  }
 
-    const std::string get_request_type() const noexcept
-    {
-        return message_->get_request_type();
-    }
+  bool is_request() const
+  {
+      return message_->is_request();
+  }
 
-    bool is_request() const
-    {
-        return message_->is_request();
-    }
+  const std::string get_client_ip() const
+  {
+      if(is_session()){
+          return get_session()->get_client_ip();
+      }
+      return get_peer()->get_ip();
+  }
 
-    const std::string get_client_ip() const
-    {
-        if(is_session()){
-            return get_session()->get_client_ip();
-        }
-        return get_peer()->get_ip();
-    }
+  void set_time_point(){
+      start_ = std::chrono::steady_clock::now();
+  }
 
-    void set_time_point(){
-        start_ = std::chrono::steady_clock::now();
-    }
+  auto get_time_point(){
+      return start_;
+  }
 
-    auto get_time_point(){
-        return start_;
-    }
+  auto is_valid(){
+      std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+      auto dur = std::chrono::duration_cast<std::chrono::seconds>(end - start_).count();
+      //at startup duration is somehow > 1000 so we filter by the && statement
+      //TODO: find why dur is so bit at the beginning
+      if(dur > 10){
+          return false;
+      }
+      return true;
+  }
 
-    auto is_valid(){
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        auto dur = std::chrono::duration_cast<std::chrono::seconds>(end - start_).count();
-        //at startup duration is somehow > 1000 so we filter by the && statement
-        //TODO: find why dur is so bit at the beginning
-        if(dur > 10){
-            return false;
-        }
-        return true;
-    }
-
-    MessageType type_;
+  MessageType type_;
 
 private:
-    static void set_connection(RequestObject& req, std::variant<Peer, SessionPtr> connection)
-    {
-        if(std::holds_alternative<SessionPtr>(connection)){
-            req.set_connection(std::get<SessionPtr>(connection));
-        } else {
-            req.set_connection(std::make_shared<Peer>(std::get<Peer>(connection)));
-        }
-    }
+  static void set_connection(RequestObject& req, std::variant<Peer, SessionPtr> connection)
+  {
+      if(std::holds_alternative<SessionPtr>(connection)){
+          req.set_connection(std::get<SessionPtr>(connection));
+      } else {
+          req.set_connection(std::make_shared<Peer>(std::get<Peer>(connection)));
+      }
+  }
 
-    MessagePtr message_;
-    std::optional<HandlerFunction> handler_;
-    std::optional<DataPromise> data_promise_deprecated_; //deprecated
-    std::variant<PeerPtr, SessionPtr> connection_;
-    std::chrono::steady_clock::time_point start_;
+  MessagePtr message_;
+  std::optional<HandlerFunctionDeprecated> handler_;
+  std::optional<DataPromise> data_promise_deprecated_; //deprecated
+  std::optional<HandlerObject<HandlerFunction>> handler_object_;
+  std::variant<PeerPtr, SessionPtr> connection_;
+  std::chrono::steady_clock::time_point start_;
 };
