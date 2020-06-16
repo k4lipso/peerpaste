b97616657345ed8280417d5aad305f8952431445
kalipso <kalipso@c3d2.de>
2020-06-17 00:48:08 +0200

    re-enable testing

--- shell.nix
+++ shell.nix
@@ -4,5 +4,5 @@ stdenv.mkDerivation {
   name = "env";
   nativeBuildInputs = [ pkgconfig python3 python37Packages.klein cmake gnumake42 lldb gdb ];
   depsBuildBuild = [ gcc9 ccls ];
-  buildInputs = [ protobuf3_7 boost172 cryptopp boost-build libsodium doxygen ];
+  buildInputs = [ protobuf3_7 boost172 cryptopp boost-build libsodium doxygen catch2 ];
 }
