515ba2d78729e3c965616eeee9cafed5631a553f
kalipso <kalipso@c3d2.de>
2020-10-23 00:59:53 +0200

    move shell.nix to default.nix and pin nixpkgs version

--- shell.nix
+++
@@ -1,8 +0,0 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation {
-  name = "env";
-  nativeBuildInputs = [ pkgconfig python3 python37Packages.klein cmake gnumake42 lldb gdb ];
-  depsBuildBuild = [ gcc9 ccls ];
-  buildInputs = [ protobuf3_7 boost172 cryptopp boost-build libsodium doxygen catch2 ];
-}
