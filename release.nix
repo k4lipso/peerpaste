9b94bd1e473139eddd9e4f5def7a6d6142621ac4
kalipso <kalipso@c3d2.de>
2020-12-02 21:17:50 +0100

    remove pinning

--- release.nix
+++ release.nix
@@ -1,12 +1,7 @@
 { ... }:
 
 let
-  pkgs = (import (builtins.fetchGit {
-    name = "nixos-unstable-2020-10-23";
-    url = "https://github.com/nixos/nixpkgs/";
-    ref = "refs/heads/nixos-unstable";
-    rev = "24eb3f87fc610f18de7076aee7c5a84ac5591e3e";
-  }) {});
+  pkgs = (import <nixpkgs> {});
 
   compilers = with pkgs; {
     gcc9 = stdenv;
