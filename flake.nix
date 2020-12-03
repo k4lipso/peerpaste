13434b598ac773647cac67a8d1b2e1e4988a9dbf
kalipso <kalipso@c3d2.de>
2020-12-03 23:48:46 +0100

    set nix flakes architectures

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
 
   outputs = { self, nixpkgs, utils }:
 
-    utils.lib.eachDefaultSystem
+    utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ]
       (system:
       let 
         pkgs = nixpkgs.legacyPackages.${system}; 
