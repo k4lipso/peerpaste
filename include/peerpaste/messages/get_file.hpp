ca9aee486ccc7f37c85ec5c23c3f44ee069cf453
kalipso <kalipso@c3d2.de>
2020-12-05 01:54:36 +0100

    extend new get file prototype
    now files are transmitted in binary chunks and dont have to be loaded completely
    in memory before transmission

--- include/peerpaste/messages/get_file.hpp
+++ include/peerpaste/messages/get_file.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <optional>
+#include <fstream>
+#include <array>
 
 #include "peerpaste/messaging_base.hpp"
 #include "peerpaste/storage.hpp"
@@ -11,7 +13,7 @@ namespace peerpaste::message
 class GetFile : public MessagingBase, public Awaitable<MESSAGE_STATE>
 {
 public:
-	GetFile(StaticStorage *storage, Peer target, std::string file_name);
+	GetFile(StaticStorage *storage, Peer target, peerpaste::FileInfo file_name);
 	GetFile(StaticStorage *storage, RequestObject request);
 	explicit GetFile(GetFile &&other);
 
@@ -25,9 +27,18 @@ private:
 	void handle_response(RequestObject request_object) override;
 	void handle_failed() override;
 
+	void write_buffer();
+	void write_file(bool failed);
+
+
 	StaticStorage *storage_;
 	std::optional<Peer> target_;
-	std::optional<std::string> file_name_;
+	std::optional<peerpaste::FileInfo> file_info_;
+	std::ifstream m_source_file;
+	std::ofstream m_output_file;
+	static constexpr size_t m_buffer_size = 1024;
+	std::array<char, m_buffer_size> m_buf;
+	size_t m_file_size = 0;
 };
 
 } // namespace peerpaste::message
