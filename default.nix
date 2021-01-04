775d39f98ad0d34e5cd0b5d57da18370732af72c
kalipso <kalipso@c3d2.de>
2021-01-04 14:39:44 +0100

    Adding sqlite_modern_cpp
    it got added as submodule since external_project and fetch_content are
    not supported by nix-build

--- default.nix
+++ default.nix
@@ -4,5 +4,5 @@ with pkgs;
 mkShell {
   nativeBuildInputs = [ pkgconfig python3 python37Packages.klein cmake gnumake42 lldb gdb ];
   depsBuildBuild = [ ccls ];
-  buildInputs = [ protobuf3_7 boost172 cryptopp clang-tools boost-build libsodium doxygen catch2 ];
+  buildInputs = [ sqlite protobuf3_7 boost172 cryptopp clang-tools boost-build libsodium doxygen catch2 ];
 }
